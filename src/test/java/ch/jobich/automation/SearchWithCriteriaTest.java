e173e5ca26907e5799dbef5e256f3916d632f03d
Anatolii <anatoleyz@gmail.com>
2026-09-01 19:29:56 +0200

    fallout changes tracked to be rolled back in another branch

---
+++ src/test/java/ch/jobich/automation/SearchWithCriteriaTest.java
@@ -0,0 +1,30 @@
+package ch.jobich.automation;
+
+import ch.jobich.automation.base.BaseUiTest;
+import ch.jobich.automation.config.ConfigReader;
+import ch.jobich.automation.data.SearchCriteria;
+import ch.jobich.automation.enums.Domain;
+import ch.jobich.automation.enums.LanguageRegion;
+import ch.jobich.automation.enums.LookingFor;
+import ch.jobich.automation.pages.HomePage;
+import ch.jobich.automation.pages.SearchResultsPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchWithCriteriaTest extends BaseUiTest {
+
+  @Test
+  public void searchByCriteriaReturnsResults() {
+    HomePage homePage = openHomePage();
+
+    SearchCriteria criteria = SearchCriteria.builder()
+          .query("engineer")
+          .region(LanguageRegion.DE)
+          .withFilter(LookingFor.REMOTE)
+          .build();
+
+    SearchResultsPage results = homePage.clickSearchJobs().search(criteria);
+
+    Assert.assertTrue(results.currentUrl().contains("search"));
+  }
+}
